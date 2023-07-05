99af7f0aac6563f1a7c154cac127462524d2e2f3
Alexis Navarro <navarroalexis007@gmail.com>
2023-07-05 17:13:02 -0600

    adding imports for the loginPage in forms & creating the loginpage in the index file

--- src/scenes/loginPage/index.jsx
+++ src/scenes/loginPage/index.jsx
@@ -1,5 +1,38 @@
+import {Box, Typography, useTheme, useMediaQuery} from "@mui/material";
+import Form from "./Form";
+
 const LoginPage =  () => {
-    return (<div>LoginPage</div>);
+    const theme = useTheme();
+    const isNonmobileScreens = useMediaQuery("(min-width: 1000px)");
+    return <Box>
+        <Box width = "100%" backgroundColor ={theme.palette.background.alt} p= "1rem 6%" textAlign = "center">
+        <Typography
+                fontWeight="bold"
+                fontSize="32px"
+                color="primary"
+                sx={{
+                    "&:hover": {
+                        color: primaryLight,
+                        cursor: "pointer",
+                    },
+                }}
+            >
+                Sociopedia
+            </Typography>
+        </Box>
+        <Box
+        width={isNonmobileScreens? "50%":"93%"}
+        p = "2rem"
+        m = "2rem auto"
+        borderRadius="1.5rem"
+        backgroundColor = {theme.palette.background.alt}
+        >
+            <Typography fontWeight= "500" variant = "h5" sx = {{mb: "1.5rem"}}>
+                welcome to Sociopedia, the social media for sociopaths
+            </Typography>
+
+        </Box>
+    </Box>
 }
 
 export default LoginPage;
